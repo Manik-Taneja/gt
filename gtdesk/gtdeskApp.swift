04685946fa488fdd5567645df2e107db1632f2d2
Manik Taneja <maniktaneja712@gmail.com>
2023-03-18 20:09:04 -0500

    Initial Commit

---
+++ gtdesk/gtdeskApp.swift
@@ -0,0 +1,17 @@
+//
+//  gtdeskApp.swift
+//  gtdesk
+//
+//  Created by Manik Taneja on 3/18/23.
+//
+
+import SwiftUI
+
+@main
+struct gtdeskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
